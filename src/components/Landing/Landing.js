21c05474a0bad3fd6da82d309805541eb9b858dd
Mohamed <mhassankd35@gmail.com>
2022-07-22 15:36:30 -0500

    adds app logo

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,16 +3,22 @@ import "./Landing.css";
 
 function Landing() {
   return (
-    <div className="landing_container">
-      <div className="header">
-        <div className="join_game">Join Game</div>
-        <div className="create_game">Create Game</div>
+    <div>
+      <div className="logo">
+        <p>Codewords</p>
+        <div>Clone</div>
       </div>
-      <div className="username">
-        <input type="text" placeholder=" username" />
-      </div>
-      <div className="room_name">
-        <input type="text" placeholder="room name" />
+      <div className="landing_container">
+        <div className="header">
+          <div className="join_game">Join Game</div>
+          <div className="create_game">Create Game</div>
+        </div>
+        <div className="username">
+          <input type="text" placeholder=" username" />
+        </div>
+        <div className="room_name">
+          <input type="text" placeholder="room name" />
+        </div>
       </div>
     </div>
   );
